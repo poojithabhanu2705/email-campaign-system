1c77377a7462e5ceea38da9abe170ba117f079a3
poojithabhanu2705 <poojithabhanuathm@gmail.com>
2026-06-24 21:27:34 +0530

    fix:real bad stuff

--- utils/mailer.js
+++ utils/mailer.js
@@ -6,7 +6,7 @@ dotenv.config();
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail({ to, subject, text, html, from }) {
-  const sender = from || process.env.RESEND_FROM || process.env.EMAIL_USER || 'no-reply@resend.dev';
+  const sender = from || process.env.RESEND_FROM || 'onboarding@resend.dev';
   return await resend.emails.send({
     from: sender,
     to,
